a374399ad12c1339eebe9a376f01f7807430f1cb
Sherin5 <sherintrades@gmail.com>
2023-07-30 19:46:00 +0530

    UPdated logging and exception classes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from Housing.logger import logging
 app = Flask(__name__)
+
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    return "Starting machine learning project"
+    logging.info("We are testing the logging module")
+    return "Logging module done"
 
 if __name__ =="__main__":
     app.run(debug=True)
